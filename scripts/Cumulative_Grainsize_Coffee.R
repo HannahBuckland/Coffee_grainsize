7915c91dbee1c49b1071834b9fa412ed0dac5265
Hannah Buckland <hannah.buckland@bristol.ac.uk>
2021-10-29 13:57:12 +0100

    added some comments to script

--- scripts/Cumulative_Grainsize_Coffee.R
+++ scripts/Cumulative_Grainsize_Coffee.R
@@ -1,33 +1,37 @@
 # Script to process raw CX2 files
 # Hannah Buckland 31/07/2021
 
-# Read in .xle files
-
+# Loading required R packages 
 
 library(tidyverse)
 library(data.table)
 library(patchwork)
 library(viridis)
 
+# First - Read in .xle files (excel compatible files that the Camsizer produces) --------
+
+
 # List all files witl .xle extension
 all_xle <- list.files("Camsizer_raw",pattern=".xle",recursive = TRUE, full.names = TRUE)
 
 # Extract ids for each analysis from file name
-samplename <- str_extract(all_xle,"(?<=Camsizer_raw/).*(?=/)")
+samplename <- str_extract(all_xle,"(?<=Camsizer_raw/).*(?=/)") # isolates the sample name
 analysisid <- str_extract(all_xle,"(?<=Camsizer_raw/).*(?=.xle)")
-analysisid <- tstrsplit(analysisid,"/")[[2]]
+analysisid <- tstrsplit(analysisid,"/")[[2]] # then we get the long analysis name (there are repeat runs for multiple samples and different size parameters)
 
+# need to isolate the size parameter for each analysis (see Buckland et al. 2021 for definitions)
 paramid <- ifelse(grepl("x_area",analysisid),"x_area",
                   ifelse(grepl("xc_min",analysisid),"xc_min",
                          "xFe_max"))
 
+# designating simplified name for each analysis (will have replicates for repeated analyses)
 simple_name <- paste0(samplename,"_",paramid)                                
 
 
 # Function to read in .xle files
 file_process <- function(xlefile) {
   
-  # need to skip nulls and set the file encoding because of strange format from CX2
+  # need to skip nulls and set the file encoding because of strange format output directly from the CX2
   dat <- read.table(xlefile,
                     fill=TRUE,
                     skipNul = TRUE,
@@ -45,10 +49,10 @@ file_process <- function(xlefile) {
   dat_clean <- dat_clean %>%
     mutate_if(is.character,as.numeric) %>%
     arrange(desc(min_um)) %>% # sort by descending size
-    mutate(cum2 = cumsum(vol_perc)) %>% # recalcualte cumulative sum as coarser than percentatge
+    mutate(cum2 = cumsum(vol_perc)) %>% # recalcualte cumulative sum as coarser than percentage
     arrange(min_um) %>% # put back to original order
     mutate(min_um = ifelse(min_um==0,0.8,
-                           min_um)) # set minimum particle size of CX2 instrument
+                           min_um)) # set minimum particle size of CX2 instrument which is 0.8 µm (see Buckland et al. 2021)
   
   return(dat_clean) # return the cleaned data from the function
   
@@ -72,7 +76,14 @@ xlefiles_processed <- xlefiles_bind %>%
          grind = paste0("grind",tstrsplit(sample,bean)[[2]]),
          grind = factor(grind,levels=c("grind01","grind02","grind03","grind04","grind05","grind06","grind07","grind08","grind09","grind10","grind11")))
 
-# Nice colour pallete in rough coffee colours
+###################################  TO DO!!!!! ################################
+# I still haven't coded up a way to deal with replicated analyses so at the moment the results aren't average
+# This is on my TO DO list
+###############################################################################
+
+# Plotting code below this line ------------
+
+# Setting up colour palette in coffee(ish) colours
 pallete <- c(grind01="#edc4b3",
              grind02="#e6b8a2",
              grind03="#deab90",
@@ -84,6 +95,9 @@ pallete <- c(grind01="#edc4b3",
              grind09="#8a5a44",
              grind10="#774936",
              grind11="#1d0200")
+
+
+# Produce plot to demonstrate how grind setting relates to the actual GSD
 
 Cplots <- ggplot(data=xlefiles_processed %>%
                   filter(param %in% c("xFemax","xcmin"),
@@ -102,8 +116,10 @@ Cplots <- ggplot(data=xlefiles_processed %>%
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank()) 
 
+# Facet by the size parameter and give title
 grindsize <- Cplots + facet_wrap(~param) + ggtitle("Grind size to grain size")
 
+# Produce plot to demonstrate how the bean type influences the GSD produced by the same grind setting
 
 Bplots <- ggplot(data=xlefiles_processed %>%
                    filter(param %in% c("xFemax","xcmin"))) +
@@ -121,9 +137,10 @@ Bplots <- ggplot(data=xlefiles_processed %>%
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank()) 
 
+# Facet by the size parameter and give title
 beantype <- Bplots + facet_wrap(~param) + ggtitle("Comparing bean types")
 
-
+# Save individual plots as pdfs
 ggsave("plots/grind_size_linear.pdf",grindsize, width = 8,height=6,units = "in")
 ggsave("plots/beantype_linear.pdf",beantype, width = 8,height=6,units = "in")
 
